b6a1fa53616fd16f608548988fc98d3aa3ba4fb6
writes <jonathonhthompson@gmail.com>
2017-09-30 15:08:22 -0500

    feat(index): add markup

---
+++ pong/Main.js
@@ -0,0 +1,22 @@
+var canvas;
+var stage;
+ 
+
+// Background
+var bg; 
+
+// Title View
+var main; //The Main Background
+var startB; //The Start button in the main menu
+var creditsB; //The credits button in the main menu
+
+// Credits View
+var credits; 
+
+// Main View
+var player; //The player paddle graphic
+var ball; //The ball graphic
+var cpu; //The CPU paddle
+var win; //The winning popup
+var lose; //The losing popup
+
